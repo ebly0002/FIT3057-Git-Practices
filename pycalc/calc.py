b773b6ff1aa71867d26e1ef95f0c8b329a932012
ebly0002 <ebly0002@student.monash.edu>
2026-04-01 19:02:27 +1100

    Initial creation of the pycalc folder, the calc.py file and compute method

---
+++ pycalc/calc.py
@@ -0,0 +1,11 @@
+def compute(expression: str):
+    values = expression.split(" ")
+    num0 = int(values[0])
+    operator = values[1]
+    num1 = int(values[2])
+
+    if operator == "+":
+        return num0 + num1
+    else:
+        print("Unknown operation!")
+        return 0
